aee8ff6a8f5111fe4a23a8eb2e36f05aa5d76558
Maura Oray <moray@brandeis.edu>
2018-11-06 18:45:02 -0500

    Update calorie.java
    Updated

--- calorie.java
+++ calorie.java
@@ -1,3 +1,12 @@
 public class calorie {
   public static void main(String[] args){
     welcome();
+  }
+
+    public static void welcome() {
+      System.out.println("Welcome to Calorie Counter!");
+      System.out.println("Please enter an item for breakfast: ");
+      String breakfast = TextIO.getln();
+      System.out.printf("You have entered %s%n", breakfast);
+    }
+}
