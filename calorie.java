827cf0e2ede26ac88dc0741de08f523d09e5a8ba
sunnyako <khurshedmo@Khurshedmos-MacBook-Air.local>
2018-12-02 13:26:15 -0500

    brackets

--- calorie.java
+++ calorie.java
@@ -8,3 +8,5 @@ public class calorie {
       System.out.println("Please choose an item for breakfast: eggs, bacon, bread, cereal, bagel, pancakes, tator totts");
       String breakfast = TextIO.getln();
       System.out.printf("You have entered %s%n", breakfast);
+    }
+}
