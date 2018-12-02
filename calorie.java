ccf94dfc57baa04334bd5fe1017e9272c3204fcd
ayzheng <ayzheng@brandeis.edu>
2018-12-02 13:18:49 -0500

    Update calorie.java
    commit

--- calorie.java
+++ calorie.java
@@ -5,7 +5,7 @@ public class calorie {
 
     public static void welcome() {
       System.out.println("Welcome to Calorie Counter!");
-      System.out.println("Please enter an item for breakfast: ");
+      System.out.println("Please choose an item for breakfast: ");
       String breakfast = TextIO.getln();
       System.out.printf("You have entered %s%n", breakfast);
     }
