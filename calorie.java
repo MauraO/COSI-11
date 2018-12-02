990c5adbacf686b4e18c101884adb1249a47f72b
sunnyako <khurshedmo@Khurshedmos-MacBook-Air.local>
2018-12-02 13:28:43 -0500

    line format

--- calorie.java
+++ calorie.java
@@ -5,7 +5,8 @@ public class calorie {
 
     public static void welcome() {
       System.out.println("Welcome to Calorie Counter!");
-      System.out.println("Please choose an item for breakfast: eggs, bacon, bread, cereal, bagel, pancakes, tator totts");
+      System.out.println("Please choose an item for breakfast: eggs, bacon, bread,
+      cereal, bagel, pancakes, tator totts");
       String breakfast = TextIO.getln();
       System.out.printf("You have entered %s%n", breakfast);
     gbgbgb
