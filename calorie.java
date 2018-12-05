8ccd84a2488b3c9dac9f4984bd96dc59c51aa5df
Maura Oray <moray@brandeis.edu>
2018-12-04 19:33:10 -0500

    Update calorie.java
    updates

--- calorie.java
+++ calorie.java
@@ -1,12 +1,14 @@
 public class calorie {
   public static void main(String[] args){
+
     welcome();
   }
 
+
     public static void welcome() {
       System.out.println("Welcome to Calorie Counter!");
-<<<<<<< HEAD
-      System.out.println("Please choose an item for breakfast: egg, banana, oatmeal, ");
+      System.out.println("Please choose an item for breakfast: eggs, bacon, bread,
+      cereal, bagel, pancakes, tater tots");
       String breakfast = TextIO.getln();
       System.out.printf("You have entered %s%n", breakfast);
 
